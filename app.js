7c0e4bd75724a040919f0061668c379e9d806aa1
TanayTejaswi <tanaytejaswi49@gmail.com>
2026-05-14 11:25:50 +0530

    Chnages commit

--- app.js
+++ app.js
@@ -33,12 +33,13 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // MySQL Connection (using the latest password provided in your files)
-const connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	database: "watchlist",
-	password: "Tejaswi49!",
-	multipleStatements: true,
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  multipleStatements: true,
 });
 
 connection.connect((err) => {
